8f387c20c185d901ca1c9e69cbcd4f588863f3cd
Tharun James <xplorejames@users.noreply.github.com>
2020-04-04 14:17:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -304,7 +304,7 @@ io.on('connection', function(socket){
 //Server configuration stuff
 
 //Tries to find variable for openshift ip, if nothing then loads to localhost
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 
 //Same as above, but with port
 //If you want to load on localhost onto a different port, change 4000 to whatever port you please
